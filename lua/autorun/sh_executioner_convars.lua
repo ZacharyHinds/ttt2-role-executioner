7bdd491332c1053ce1b7442ba9085945c7608173
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2020-06-29 14:11:45 -0400

    Update sh_executioner_convars.lua
    Added Replicated flag to multiplier convars

--- lua/autorun/sh_executioner_convars.lua
+++ lua/autorun/sh_executioner_convars.lua
@@ -1,6 +1,6 @@
 CreateConVar("ttt2_executioner_punishment_time", 60, {FCVAR_ARCHIVE, FCVAR_NOTIFY})
-CreateConVar("ttt2_executioner_target_multiplier", 2, {FCVAR_ARCHIVE, FCVAR_NOTIFY})
-CreateConVar("ttt2_executioner_non_target_multiplier", 0.5, {FCVAR_ARCHIVE, FCVAR_NOTIFY})
+CreateConVar("ttt2_executioner_target_multiplier", 2, {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED})
+CreateConVar("ttt2_executioner_non_target_multiplier", 0.5, {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED})
 
 hook.Add("TTTUlxDynamicRCVars", "ttt2_ulx_dynamic_executioner_convars", function(tbl)
   tbl[ROLE_EXECUTIONER] = tbl[ROLE_EXECUTIONER] or {}
