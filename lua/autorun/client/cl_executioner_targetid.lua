df7e1181a0186d09ccd0b99046a113c8f8011c84
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2020-06-30 13:58:49 -0400

    Update cl_executioner_targetid.lua
    removed excessive precision

--- lua/autorun/client/cl_executioner_targetid.lua
+++ lua/autorun/client/cl_executioner_targetid.lua
@@ -17,7 +17,7 @@ hook.Add("TTTRenderEntityInfo", "ttt2_executioner_target_highlight", function(tD
 
   if ply:GetTargetPlayer() == ent then
     tData:AddDescriptionLine(
-      LANG.GetParamTranslation("ttt2_executioner_player_target", {multiplier = GetConVar("ttt2_executioner_target_multiplier"):GetFloat()}),
+      LANG.GetParamTranslation("ttt2_executioner_player_target", {multiplier = math.Round(GetConVar("ttt2_executioner_target_multiplier"):GetFloat(), 1)}),
       EXECUTIONER.ltcolor
     )
 
@@ -27,7 +27,7 @@ hook.Add("TTTRenderEntityInfo", "ttt2_executioner_target_highlight", function(tD
     )
   else
     tData:AddDescriptionLine(
-      LANG.GetParamTranslation("ttt2_executioner_player_nontarget", {multiplier = GetConVar("ttt2_executioner_non_target_multiplier"):GetFloat()}),
+      LANG.GetParamTranslation("ttt2_executioner_player_nontarget", {multiplier = math.Round(GetConVar("ttt2_executioner_non_target_multiplier"):GetFloat(), 1)}),
       COLOR_WHITE
     )
   end
