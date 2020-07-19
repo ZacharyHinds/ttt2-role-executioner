9de9f1b706e2eb0a4194f8e5b04370f56763a23d
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2020-07-18 21:07:07 -0400

    Added Addon Dev Emblem Code

--- lua/autorun/sh_executioner_convars.lua
+++ lua/autorun/sh_executioner_convars.lua
@@ -2,6 +2,10 @@ CreateConVar("ttt2_executioner_punishment_time", 60, {FCVAR_ARCHIVE, FCVAR_NOTIF
 CreateConVar("ttt2_executioner_target_multiplier", 2, {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED})
 CreateConVar("ttt2_executioner_non_target_multiplier", 0.5, {FCVAR_ARCHIVE, FCVAR_NOTIFY, FCVAR_REPLICATED})
 
+hook.Add("TTT2FinishedLoading", "mes_devicon", function() -- addon developer emblem for me ^_^
+  AddTTT2AddonDev("76561198049910438")
+end)
+
 hook.Add("TTTUlxDynamicRCVars", "ttt2_ulx_dynamic_executioner_convars", function(tbl)
   tbl[ROLE_EXECUTIONER] = tbl[ROLE_EXECUTIONER] or {}
 
