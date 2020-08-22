ff69e3ac47daf47b7eb9d435b6be6741f26f0fc6
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2020-08-22 17:45:27 -0400

    Code Cleanup

--- lua/terrortown/autorun/shared/sh_executioner_convars.lua
+++ lua/terrortown/autorun/shared/sh_executioner_convars.lua
@@ -12,29 +12,29 @@ hook.Add("TTTUlxDynamicRCVars", "ttt2_ulx_dynamic_executioner_convars", function
   tbl[ROLE_EXECUTIONER] = tbl[ROLE_EXECUTIONER] or {}
 
   table.insert(tbl[ROLE_EXECUTIONER], {
-    cvar = "ttt2_executioner_punishment_time",
-    slider = true,
-    min = 0,
-    max = 240,
-    decimal = 0,
-    desc = "ttt2_executioner_punishment_time (def. 60)"
+      cvar = "ttt2_executioner_punishment_time",
+      slider = true,
+      min = 0,
+      max = 240,
+      decimal = 0,
+      desc = "ttt2_executioner_punishment_time (def. 60)"
   })
 
   table.insert(tbl[ROLE_EXECUTIONER], {
-    cvar = "ttt2_executioner_target_multiplier",
-    slider = true,
-    min = 1,
-    max = 10,
-    decimal = 1,
-    desc = "ttt2_executioner_target_multiplier (def. 2)"
+      cvar = "ttt2_executioner_target_multiplier",
+      slider = true,
+      min = 1,
+      max = 10,
+      decimal = 1,
+      desc = "ttt2_executioner_target_multiplier (def. 2)"
   })
 
   table.insert(tbl[ROLE_EXECUTIONER], {
-    cvar = "ttt2_executioner_non_target_multiplier",
-    slider = true,
-    min = 0,
-    max = 1,
-    decimal = 1,
-    desc = "ttt2_executioner_non_target_multiplier (def. 0.5)"
+      cvar = "ttt2_executioner_non_target_multiplier",
+      slider = true,
+      min = 0,
+      max = 1,
+      decimal = 1,
+      desc = "ttt2_executioner_non_target_multiplier (def. 0.5)"
   })
 end)
--- lua/terrortown/entities/roles/executioner/shared.lua
+++ lua/terrortown/entities/roles/executioner/shared.lua
@@ -1,95 +1,95 @@
 if SERVER then
-	AddCSLuaFile()
+  AddCSLuaFile()
 
-	resource.AddFile("materials/vgui/ttt/dynamic/roles/icon_exc.vmt")
+  resource.AddFile("materials/vgui/ttt/dynamic/roles/icon_exc.vmt")
 
-	-- if there is TTTC installed: sync classes
-	util.AddNetworkString("TTT2ExecutionerSyncClasses")
+  -- if there is TTTC installed: sync classes
+  util.AddNetworkString("TTT2ExecutionerSyncClasses")
 end
 
 function ROLE:PreInitialize()
-	self.color = Color(163, 15, 28, 255)
+  self.color = Color(163, 15, 28, 255)
 
-	self.abbr = "exc" -- abbreviation
-	self.surviveBonus = 0.5 -- bonus multiplier for every survive while another player was killed
-	self.scoreKillsMultiplier = 5 -- multiplier for kill of player of another team
-	self.scoreTeamKillsMultiplier = -16 -- multiplier for teamkill
-	self.preventFindCredits = false
-	self.preventKillCredits = false
-	self.preventTraitorAloneCredits = false
+  self.abbr = "exc" -- abbreviation
+  self.surviveBonus = 0.5 -- bonus multiplier for every survive while another player was killed
+  self.scoreKillsMultiplier = 5 -- multiplier for kill of player of another team
+  self.scoreTeamKillsMultiplier = -16 -- multiplier for teamkill
+  self.preventFindCredits = false
+  self.preventKillCredits = false
+  self.preventTraitorAloneCredits = false
 
-	self.defaultEquipment = SPECIAL_EQUIPMENT -- here you can set up your own default equipment
-	self.defaultTeam = TEAM_TRAITOR
+  self.defaultEquipment = SPECIAL_EQUIPMENT -- here you can set up your own default equipment
+  self.defaultTeam = TEAM_TRAITOR
 
-	self.conVarData = {
-		pct = 0.17, -- necessary: percentage of getting this role selected (per player)
-		maximum = 1, -- maximum amount of roles in a round
-		minPlayers = 6, -- minimum amount of players until this role is able to get selected
-		credits = 0, -- the starting credits of a specific role
-		togglable = true, -- option to toggle a role for a client if possible (F1 menu)
-		random = 50,
-		traitorButton = 1, -- can use traitor buttons
-		shopFallback = SHOP_FALLBACK_TRAITOR
-	}
+  self.conVarData = {
+    pct = 0.17, -- necessary: percentage of getting this role selected (per player)
+    maximum = 1, -- maximum amount of roles in a round
+    minPlayers = 6, -- minimum amount of players until this role is able to get selected
+    credits = 0, -- the starting credits of a specific role
+    togglable = true, -- option to toggle a role for a client if possible (F1 menu)
+    random = 50,
+    traitorButton = 1, -- can use traitor buttons
+    shopFallback = SHOP_FALLBACK_TRAITOR
+  }
 end
 
 -- now link this subrole with its baserole
 function ROLE:Initialize()
-	roles.SetBaseRole(self, ROLE_TRAITOR)
+  roles.SetBaseRole(self, ROLE_TRAITOR)
 end
 
 -- local h_TTT2CheckCreditAward = "TTT2HitmanSpecialCreditReward"
 local ex_TTTCPostReceiveCustomClasses = "TTT2ExecutionerCanSeeClasses"
 
 if SERVER then
-	-- TODO improve networking. If TTTC is disabled, this doesn't need to be handled
-	local function SendClassesToExecutioner(executioner)
-		if not TTTC then return end
+  -- TODO improve networking. If TTTC is disabled, this doesn't need to be handled
+  local function SendClassesToExecutioner(executioner)
+    if not TTTC then return end
 
-		for _, ply in ipairs(player.GetAll()) do
-			if ply ~= executioner then
-				net.Start("TTT2ExecutionerSyncClasses")
-				net.WriteEntity(ply)
-				net.WriteUInt(ply:GetCustomClass() or 0, CLASS_BITS)
-				net.Send(executioner)
-			end
-		end
-	end
+    for _, ply in ipairs(player.GetAll()) do
+      if ply ~= executioner then
+        net.Start("TTT2ExecutionerSyncClasses")
+        net.WriteEntity(ply)
+        net.WriteUInt(ply:GetCustomClass() or 0, CLASS_BITS)
+        net.Send(executioner)
+      end
+    end
+  end
 
-	-- hook.Add("TTT2CheckCreditAward", h_TTT2CheckCreditAward, function(victim, attacker)
-	-- 	if IsValid(attacker) and attacker:IsPlayer() and attacker:IsActive() and attacker:GetSubRole() == ROLE_HITMAN then
-	-- 		return false -- prevent awards
-	-- 	end
-	-- end)
+  -- hook.Add("TTT2CheckCreditAward", h_TTT2CheckCreditAward, function(victim, attacker)
+  -- 	if IsValid(attacker) and attacker:IsPlayer() and attacker:IsActive() and attacker:GetSubRole() == ROLE_HITMAN then
+  -- 		return false -- prevent awards
+  -- 	end
+  -- end)
 
-	hook.Add("TTT2UpdateSubrole", ex_TTTCPostReceiveCustomClasses, function(executioner, oldRole, role)
-		if not TTTC then return end
+  hook.Add("TTT2UpdateSubrole", ex_TTTCPostReceiveCustomClasses, function(executioner, oldRole, role)
+    if not TTTC then return end
 
-		if executioner:IsActive() and role == ROLE_EXECUTIONER then
-			SendClassesToExecutioner(executioner)
-		end
-	end)
+    if executioner:IsActive() and role == ROLE_EXECUTIONER then
+      SendClassesToExecutioner(executioner)
+    end
+  end)
 
-	hook.Add("TTTCPostReceiveCustomClasses", ex_TTTCPostReceiveCustomClasses, function()
-		if not TTTC then return end
+  hook.Add("TTTCPostReceiveCustomClasses", ex_TTTCPostReceiveCustomClasses, function()
+    if not TTTC then return end
 
-		for _, executioner in ipairs(player.GetAll()) do
-			if executioner:IsActive() and executioner:GetSubRole() == ROLE_EXECUTIONER then
-				SendClassesToExecutioner(executioner)
-			end
-		end
-	end)
+    for _, executioner in ipairs(player.GetAll()) do
+      if executioner:IsActive() and executioner:GetSubRole() == ROLE_EXECUTIONER then
+        SendClassesToExecutioner(executioner)
+      end
+    end
+  end)
 end
 
 if CLIENT then
-	net.Receive("TTT2ExecutionerSyncClasses", function(len)
-		local target = net.ReadEntity()
-		local class = net.ReadUInt(CLASS_BITS)
+  net.Receive("TTT2ExecutionerSyncClasses", function(len)
+    local target = net.ReadEntity()
+    local class = net.ReadUInt(CLASS_BITS)
 
-		if class == 0 then
-			class = nil
-		end
+    if class == 0 then
+      class = nil
+    end
 
-		target:SetClass(class)
-	end)
+    target:SetClass(class)
+  end)
 end
